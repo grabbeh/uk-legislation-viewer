d6a03cbd7efeab5b987d6aab4da28b70380b1f9d
Michael <mbg@outlook.com>
2020-08-13 14:02:18 +0100

    Deploy

--- src/components/deploy.js
+++ src/components/deploy.js
@@ -4,7 +4,7 @@ import { jsx, Box } from 'theme-ui'
 const Deploy = () => {
   return (
     <Box>
-      <a href='https://app.netlify.com/start/deploy?repository=https://www.netlify.com/img/deploy/button.svg'>
+      <a href='https://app.netlify.com/start/deploy?repository=https://github.com/grabbeh/uk-legislation-viewer'>
         <img src='https://www.netlify.com/img/deploy/button.svg' />
       </a>
     </Box>
